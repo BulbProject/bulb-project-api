cef17ffeed81b98bacf35e897d2fbb3f8948f047
Dmitriy Volkov <mr.rotberry@gmail.com>
2020-03-23 18:26:25 +0200

    feat(routes) Add routes structure

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import fastify from 'fastify';
 
+import routes from 'routes';
+
 import { serviceConfig } from 'config';
 
 const app = fastify({ logger: true, ignoreTrailingSlash: true });
+
+routes.forEach(route => route(app));
 
 const start = async (): Promise<void> => {
   try {
