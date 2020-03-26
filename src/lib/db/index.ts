90ef0af9448cc209e30af31988a405d0319947f3
Dmitriy Volkov <mr.rotberry@gmail.com>
2020-03-26 18:59:29 +0200

    feat(project): Add aliases and change db config

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import { dbConfig } from 'config';
 
 export const connectToDb = async (): Promise<void> => {
+  mongoose.set('useCreateIndex', true);
+
   await mongoose.connect(`mongodb://${dbConfig.host}:${dbConfig.port}/${dbConfig.name}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
