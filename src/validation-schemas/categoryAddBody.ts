0fda8bb42744686f51680235574cfb7459f64a61
Dmitriy Volkov <mr.rotberry@gmail.com>
2020-03-30 22:15:40 +0300

    fix(Validation): Add validation to empty body

--- src/validation-schemas/categoryAddBody.ts
+++ src/validation-schemas/categoryAddBody.ts
@@ -37,126 +37,129 @@ const classification = (scheme = 'CPV') =>
     })
     .required();
 
-export const categoryAddBodySchema = object().shape({
-  id,
-  title,
-  description: string().required(),
-  classification: classification(),
-  items: array()
-    .of(
-      object().shape({
-        id,
-        description: string().required(),
-        classification: classification(),
-        additionalClassifications: array()
-          .of(classification('ProzorroMarketProfile'))
-          .min(1)
-          .required(),
-      })
-    )
-    .min(1)
-    .required(),
-  criteria: array()
-    .of(
-      object().shape({
-        id,
-        title,
-        description: string(),
-        relatesTo: string()
-          .oneOf(['item'])
-          .required(),
-        relatedItem: string().required(),
-        requirementGroups: array()
-          .of(
-            object().shape({
-              id,
-              description: string(),
-              requirements: array()
-                .of(
-                  object().shape({
-                    id,
-                    title,
-                    description: string(),
-                    dataType: string()
-                      .oneOf(['boolean', 'string', 'number', 'integer'])
-                      .required(),
-                    expectedValue: mixed()
-                      .when('dataType', {
-                        is: dataType => dataType === 'boolean',
-                        then: boolean().required(),
-                      })
-                      .when('dataType', {
-                        is: dataType => dataType === 'string',
-                        then: string(),
-                      })
-                      .when('dataType', {
-                        is: dataType => dataType === 'number',
-                        then: number(),
-                      })
-                      .when('dataType', {
-                        is: dataType => dataType === 'integer',
-                        then: number().integer(),
-                      }),
-                    minValue: mixed()
-                      .when('dataType', {
-                        is: dataType => dataType === 'number',
-                        then: number(),
-                      })
-                      .when('dataType', {
-                        is: dataType => dataType === 'integer',
-                        then: number().integer(),
-                      }),
-                    maxValue: mixed()
-                      .when('dataType', {
-                        is: dataType => dataType === 'number',
-                        then: number(),
-                      })
-                      .when('dataType', {
-                        is: dataType => dataType === 'integer',
-                        then: number().integer(),
-                      }),
-                    optionDetails: mixed()
-                      .when('dataType', {
-                        is: dataType => dataType === 'string',
-                        then: optionDetails(string().required()),
-                      })
-                      .when('dataType', {
-                        is: dataType => dataType === 'number' || dataType === 'integer',
-                        then: optionDetails(number().required()),
-                      }),
-                  })
-                )
-                .min(1)
-                .required(),
-            })
-          )
-          .min(1)
-          .required(),
-      })
-    )
-    .min(1)
-    .required(),
-  conversions: array()
-    .of(
-      object().shape({
-        id,
-        relatesTo: string()
-          .matches(/requirement/)
-          .required(),
-        relatedItem: string().required(),
-        description: string(),
-        coefficients: array()
-          .of(
-            object().shape({
-              id,
-              value: string().required(),
-              coefficient: number().required(),
-            })
-          )
-          .min(1)
-          .required(),
-      })
-    )
-    .min(1)
-    .required(),
-});
+export const categoryAddBodySchema = object()
+  .shape({
+    id,
+    title,
+    description: string().required(),
+    classification: classification(),
+    items: array()
+      .of(
+        object().shape({
+          id,
+          description: string().required(),
+          classification: classification(),
+          additionalClassifications: array()
+            .of(classification('ProzorroMarketProfile'))
+            .min(1)
+            .required(),
+        })
+      )
+      .min(1)
+      .required(),
+    criteria: array()
+      .of(
+        object().shape({
+          id,
+          title,
+          description: string(),
+          relatesTo: string()
+            .oneOf(['item'])
+            .required(),
+          relatedItem: string().required(),
+          requirementGroups: array()
+            .of(
+              object().shape({
+                id,
+                description: string(),
+                requirements: array()
+                  .of(
+                    object().shape({
+                      id,
+                      title,
+                      description: string(),
+                      dataType: string()
+                        .oneOf(['boolean', 'string', 'number', 'integer'])
+                        .required(),
+                      expectedValue: mixed()
+                        .when('dataType', {
+                          is: dataType => dataType === 'boolean',
+                          then: boolean().required(),
+                        })
+                        .when('dataType', {
+                          is: dataType => dataType === 'string',
+                          then: string(),
+                        })
+                        .when('dataType', {
+                          is: dataType => dataType === 'number',
+                          then: number(),
+                        })
+                        .when('dataType', {
+                          is: dataType => dataType === 'integer',
+                          then: number().integer(),
+                        }),
+                      minValue: mixed()
+                        .when('dataType', {
+                          is: dataType => dataType === 'number',
+                          then: number(),
+                        })
+                        .when('dataType', {
+                          is: dataType => dataType === 'integer',
+                          then: number().integer(),
+                        }),
+                      maxValue: mixed()
+                        .when('dataType', {
+                          is: dataType => dataType === 'number',
+                          then: number(),
+                        })
+                        .when('dataType', {
+                          is: dataType => dataType === 'integer',
+                          then: number().integer(),
+                        }),
+                      optionDetails: mixed()
+                        .when('dataType', {
+                          is: dataType => dataType === 'string',
+                          then: optionDetails(string().required()),
+                        })
+                        .when('dataType', {
+                          is: dataType => dataType === 'number' || dataType === 'integer',
+                          then: optionDetails(number().required()),
+                        }),
+                    })
+                  )
+                  .min(1)
+                  .required(),
+              })
+            )
+            .min(1)
+            .required(),
+        })
+      )
+      .min(1)
+      .required(),
+    conversions: array()
+      .of(
+        object().shape({
+          id,
+          relatesTo: string()
+            .matches(/requirement/)
+            .required(),
+          relatedItem: string().required(),
+          description: string(),
+          coefficients: array()
+            .of(
+              object().shape({
+                id,
+                value: string().required(),
+                coefficient: number().required(),
+              })
+            )
+            .min(1)
+            .required(),
+        })
+      )
+      .min(1)
+      .required(),
+  })
+  .nullable()
+  .required('Empty request body');
