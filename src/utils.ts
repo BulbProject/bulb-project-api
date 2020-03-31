873144385d84524145b945b373fa4926ed12338b
Dmitriy Volkov <mr.rotberry@gmail.com>
2020-03-31 12:47:39 +0300

    feat(Routes): Add functionality for update category and refactor add category

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,9 @@
 import dayjs from 'dayjs';
 
 export const formatDate = (date: Date): string => `${dayjs(date).format('YYYY-MM-DDTHH:mm:ss')}Z`;
+
+export const getLastVersion = (versions: { _id: string }[]) => {
+  return versions
+    .map(version => +version._id.replace(/^.*-v/, ''))
+    .sort((versionA, versionB) => versionB - versionA)[0];
+};
