5fb4c667a2e1bf37cbbbc3cac277c661987b3cce
Dima Volkov <mr.rotberry@gmail.com>
2020-04-01 14:19:26 +0300

    fix(Deploy): Change parameters of deploy (#15)

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ routes.forEach(route => route(app));
 
 const start = async (): Promise<void> => {
   try {
-    await app.listen({ host: serviceConfig.host, port: +serviceConfig.port });
+    await app.listen(+serviceConfig.port);
     if (process.env.NODE_ENV === 'development') console.log(app.printRoutes());
 
     await connectToDb();
