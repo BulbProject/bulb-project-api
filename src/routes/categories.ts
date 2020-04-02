2b4b33b1b6f0929ff84f124304c75880dcad6057
drizzer14 <drizzer14@gmail.com>
2020-04-02 13:28:22 +0300

    feat: Add description of categories list route

--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -2,10 +2,50 @@ import fastify from 'fastify';
 
 import { getCategoriesList, getVersionsPackage, getCategoryVersion } from 'controllers/categories';
 
+const tags = ['Categories'];
+
 export const categoriesRoute = (app: fastify.FastifyInstance): void => {
-  app.get('/categories', {}, getCategoriesList);
+  app.get(
+    '/categories',
+    {
+      schema: {
+        tags,
+        response: {
+          200: {
+            type: 'object',
+            properties: {
+              id: {
+                type: 'string',
+              },
+              version: {
+                type: 'string',
+              },
+            },
+            required: ['id', 'version'],
+          },
+        },
+      },
+    },
+    getCategoriesList
+  );
 
-  app.get('/categories/:categoryId', {}, getVersionsPackage);
+  app.get(
+    '/categories/:categoryId',
+    {
+      schema: {
+        tags,
+      },
+    },
+    getVersionsPackage
+  );
 
-  app.get('/categories/:categoryId/:version', {}, getCategoryVersion);
+  app.get(
+    '/categories/:categoryId/:version',
+    {
+      schema: {
+        tags,
+      },
+    },
+    getCategoryVersion
+  );
 };
