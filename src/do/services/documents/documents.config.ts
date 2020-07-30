c4551f5ecc082c6c88cd8a187eb938d8b4ac48e4
semenov-ol <semenov.os16@gmail.ocm>
2020-07-30 12:12:30 +0300

    refactor: Change DOCUMENTS_URL on DOCUMENTS_SERVICE

--- src/do/services/documents/documents.config.ts
+++ src/do/services/documents/documents.config.ts
@@ -12,7 +12,7 @@ interface DocumentsConfig {
 export const documentsConfig = registerAs(
   'documents',
   (): DocumentsConfig => ({
-    url: process.env.DOCUMENTS_URL || 'https://udoc.eprocurement.systems',
+    url: process.env.DOCUMENTS_SERVICE || 'https://udoc.eprocurement.systems',
     repo: {
       owner: process.env.DOCUMENTS_REPO_OWNER || 'BulbProject',
       name: process.env.DOCUMENTS_REPO_NAME || 'bulb-project-api',
