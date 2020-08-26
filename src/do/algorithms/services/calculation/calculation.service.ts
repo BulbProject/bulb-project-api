ff42cde6d1b2c9c2e82fbec5f419197db67cf089
natalia-lypovyk <cardinnata@gmail.com>
2020-08-26 13:21:26 +0300

    refactor: refactor path from `shared` and message

--- src/do/algorithms/services/calculation/calculation.service.ts
+++ src/do/algorithms/services/calculation/calculation.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 
+import { CategoryVersionRepositoryService } from 'shared/repositories/category-version';
 import { AlgorithmsService } from '../../algorithms.service';
-import { CategoryVersionRepositoryService } from '../../../../shared/repositories/category-version';
 import type { CalculationResponse } from '../../../entity/calculation';
 import { RequestedNeed } from '../../../entity/requested-need';
 
@@ -16,7 +16,7 @@ export class CalculationService {
     const categoryVersion = await this.categories.getOne([categoryId, version]);
 
     if (categoryVersion.status === 'pending') {
-      throw new BadRequestException('Calculation is impossible because category status is pending');
+      throw new BadRequestException('Calculation is impossible because category status is pending.');
     }
 
     return this.algorithms.getCalculation(categoryId, {
